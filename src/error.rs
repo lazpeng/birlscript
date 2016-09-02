55d37dced6852aade7ca1a087b5ae9cf1b7d0d77
lazpeng <lazpeng@gmail.com>
2016-09-01 22:31:10 -0300

    Consertado erro do output colorido no windows

--- src/error.rs
+++ src/error.rs
@@ -7,8 +7,13 @@ extern crate ansi_term;
 macro_rules! abort {
     ($($tt:tt)*) => {{
         use std::process::*;
-        use ansi_term::*;
-        print!("{}", Colour::Red.paint("Erro: "));
+// No windows, a crate ansi_term não faz output colorido, então só printa a mensagem comum
+        if cfg!(windows) {
+            print!("Erro: ");
+        } else {
+            use ansi_term::*;
+            print!("{}", Colour::Red.paint("Erro: "));
+        }
         println!($($tt)*);
         exit(-1);
     }}
@@ -17,8 +22,12 @@ macro_rules! abort {
 #[macro_export]
 macro_rules! warn {
     ($($tt:tt)*) => {{
-        use ansi_term::*;
-        print!("{}", Colour::Yellow.paint("Aviso: "));
+        if cfg!(windows) {
+            print!("Erro: ");
+        } else {
+            use ansi_term::*;
+            print!("{}", Colour::Yellow.paint("Aviso: "));
+        }
         println!($($tt)*);
     }}
 }
