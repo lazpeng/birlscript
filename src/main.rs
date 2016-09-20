68f66a06cca0e29d621f447afe875dc10828d4fd
lazpeng <lazpeng@gmail.com>
2016-09-20 18:49:44 -0300

    Inicio da implementação do parser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod birl;
+mod parser;
 
 fn main() {
-    println!("Hello, world!");
-	println!("{} {} ─ {}", birl::NAME, birl::VERSION, birl::GREETING);
+    println!("{} {} ─ {}", birl::NAME, birl::VERSION, birl::GREETING);
 }
