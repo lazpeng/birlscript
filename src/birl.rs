6ba97ca1e2d4c06f4c0cb6470cd807b704ecab6e
Matheus Borella (DarkRyu550) <dark.ryu.550@gmail.com>
2016-09-20 22:03:15 -0300

    Adicionado o `Type`, pra diferenciação entre valores e tipos.

--- src/birl.rs
+++ src/birl.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 pub static NAME: &'static str = "BIRL Interpreter";
 pub static VERSION: &'static str = env!("CARGO_PKG_VERSION");
 pub static GREETING: &'static str = "Aqui nóis constrói fibra.";
@@ -6,7 +8,7 @@ type Identifier = String;
 
 use std::collections::HashMap;
 pub struct Structure {
-    fields: HashMap<Identifier, Type>,
+    fields: HashMap<Identifier, Value>,
 }
 impl Structure {
     pub fn size_of(&self) -> usize {
@@ -15,10 +17,20 @@ impl Structure {
     }
 }
 
+pub enum Type{
+    Array, Str, Char, Struct, Void,
+    Unsigned8, Unsigned16, Unsigned32, Unsigned64, UnsignedSize,
+      Signed8,   Signed16,   Signed32,   Signed64,   SignedSize,
+    Float32, Float64
+}
+impl Type{
+
+}
+
 /// Possible language types, and their correspondants in Rust
-pub enum Type {
+pub enum Value {
     /// Stores a collections of other elements
-    Array(Vec<Type>),
+    Array(Vec<Value>),
     /// Owned string
     Str(String),
     /// Character
@@ -29,47 +41,54 @@ pub enum Type {
     Void,
 
     // Integer types
-    Unsigned8(u8),
-    Unsigned16(u16),
-    Unsigned32(u32),
-    Unsigned64(u64),
-    UnsignedSize(usize),
-    Signed8(i8),
-    Signed16(i16),
-    Signed32(i32),
-    Signed64(i64),
-    SignedSize(isize),
+    Unsigned8(u8), Unsigned16(u16), Unsigned32(u32), Unsigned64(u64), UnsignedSize(usize),
+      Signed8(i8), Signed16(i16),   Signed32(i32),   Signed64(i64),   SignedSize(isize),
 
     // Floats
-    Float32(f32),
-    Float64(f64),
+    Float32(f32), Float64(f64),
 }
-impl Type {
+impl Value {
+    pub fn get_type(&self) -> Type{
+        match self{
+            &Value::Array(_) => Type::Array,
+            &Value::Str(_) => Type::Str,
+            &Value::Char(_) => Type::Char,
+            &Value::Struct(_) => Type::Struct,
+
+            &Value::Void => Type::Void,
+
+            // Both signed and unsigned types have the same size in bytes
+            &Value::Unsigned8(_)    => Type::Unsigned8,    &Value::Signed8(_)    => Type::Signed8,
+            &Value::Unsigned16(_)   => Type::Unsigned16,   &Value::Signed16(_)   => Type::Signed16,
+            &Value::Unsigned32(_)   => Type::Unsigned32,   &Value::Signed32(_)   => Type::Signed32,
+            &Value::Unsigned64(_)   => Type::Unsigned64,   &Value::Signed64(_)   => Type::Signed64,
+            &Value::UnsignedSize(_) => Type::UnsignedSize, &Value::SignedSize(_) => Type::SignedSize,
+
+            &Value::Float32(_) => Type::Float32,
+            &Value::Float64(_) => Type::Float64,
+        }
+    }
+
     pub fn size_of(&self) -> usize {
         use std::mem::size_of as len;
         match self {
-            &Type::Array(ref array) => array.iter().fold(0, |size, entry| size + entry.size_of()),
-            &Type::Str(ref string) => string.len(),
-            &Type::Char(ref c) => c.len_utf8(),
-            &Type::Struct(ref structure) => structure.size_of(),
+            &Value::Array(ref array) => array.iter().fold(0, |size, entry| size + entry.size_of()),
+            &Value::Str(ref string) => string.len(),
+            &Value::Char(ref c) => c.len_utf8(),
+            &Value::Struct(ref structure) => structure.size_of(),
 
             // Internally, voids have no size
-            &Type::Void => 0,
+            &Value::Void => 0,
 
             // Both signed and unsigned types have the same size in bytes
-            &Type::Unsigned8(_) |
-            &Type::Signed8(_) => len::<u8>(),
-            &Type::Unsigned16(_) |
-            &Type::Signed16(_) => len::<u16>(),
-            &Type::Unsigned32(_) |
-            &Type::Signed32(_) => len::<u32>(),
-            &Type::Unsigned64(_) |
-            &Type::Signed64(_) => len::<u64>(),
-            &Type::UnsignedSize(_) |
-            &Type::SignedSize(_) => len::<usize>(),
+            &Value::Unsigned8(_) | &Value::Signed8(_) => len::<u8>(),
+            &Value::Unsigned16(_) | &Value::Signed16(_) => len::<u16>(),
+            &Value::Unsigned32(_) | &Value::Signed32(_) => len::<u32>(),
+            &Value::Unsigned64(_) | &Value::Signed64(_) => len::<u64>(),
+            &Value::UnsignedSize(_) | &Value::SignedSize(_) => len::<usize>(),
 
-            &Type::Float32(_) => len::<f32>(),
-            &Type::Float64(_) => len::<f64>(),
+            &Value::Float32(_) => len::<f32>(),
+            &Value::Float64(_) => len::<f64>(),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[allow(dead_code)]
 mod birl;
 mod parser;
 
