d3daf720ee15a6e119ce6954d5f778e824537013
Matheus Borella (DarkRyu550) <dark.ryu.550@gmail.com>
2016-09-23 16:10:12 -0300

    Inicial VM implementation

--- src/birl.rs
+++ src/birl.rs
@@ -7,16 +7,30 @@ pub static GREETING: &'static str = "Aqui nóis constrói fibra.";
 type Identifier = String;
 
 use std::collections::HashMap;
+#[derive(Clone, PartialEq, Debug)]
 pub struct Structure {
     fields: HashMap<Identifier, Value>,
 }
 impl Structure {
+    pub fn new() -> Structure{
+        Structure{ fields: HashMap::new() }
+    }
+
     pub fn size_of(&self) -> usize {
         // Add together all the entry sizes
         self.fields.values().fold(0, |size, entry| size + entry.size_of())
     }
 }
 
+#[derive(Clone, PartialEq, Debug)]
+pub struct Function{ function: Box<i8> }
+impl Function {
+    pub fn call() -> Type {
+        Type::Void
+    }
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub enum Type{
     Array, Str, Char, Struct, Void,
     Unsigned8, Unsigned16, Unsigned32, Unsigned64, UnsignedSize,
@@ -24,10 +38,28 @@ pub enum Type{
     Float32, Float64
 }
 impl Type{
+    pub fn default(self) -> Value{
+        match self{
+            Type::Array  => Value::Array(Vec::new()),
+            Type::Str    => Value::Str(String::new()),
+            Type::Char   => Value::Char(' '),
+            Type::Struct => Value::Struct(Structure::new()),
+            Type::Void   => Value::Void,
 
+            Type::Unsigned8    => Value::Unsigned8(0),    Type::Signed8    => Value::Signed8(0),
+            Type::Unsigned16   => Value::Unsigned16(0),   Type::Signed16   => Value::Signed16(0),
+            Type::Unsigned32   => Value::Unsigned32(0),   Type::Signed32   => Value::Signed32(0),
+            Type::Unsigned64   => Value::Unsigned64(0),   Type::Signed64   => Value::Signed64(0),
+            Type::UnsignedSize => Value::UnsignedSize(0), Type::SignedSize => Value::SignedSize(0),
+
+            Type::Float32 => Value::Float32(0.0),
+            Type::Float64 => Value::Float64(0.0),
+        }
+    }
 }
 
 /// Possible language types, and their correspondants in Rust
+#[derive(Clone, PartialEq, Debug)]
 pub enum Value {
     /// Stores a collections of other elements
     Array(Vec<Value>),
@@ -91,13 +123,105 @@ impl Value {
             &Value::Float64(_) => len::<f64>(),
         }
     }
+
+    pub fn try_cast(self, target: Type) -> Result<Value, Value>{
+        macro_rules! cast_number{
+            ($num:ident) => {
+                match target{
+                    // Cast to other number types
+                    Type::Unsigned8 => Ok(Value::Unsigned8($num as u8)), Type::Signed8 => Ok(Value::Signed8($num as i8)),
+                    Type::Unsigned16 => Ok(Value::Unsigned16($num as u16)), Type::Signed16 => Ok(Value::Signed16($num as i16)),
+                    Type::Unsigned32 => Ok(Value::Unsigned32($num as u32)), Type::Signed32 => Ok(Value::Signed32($num as i32)),
+                    Type::Unsigned64 => Ok(Value::Unsigned64($num as u64)), Type::Signed64 => Ok(Value::Signed64($num as i64)),
+                    Type::UnsignedSize => Ok(Value::UnsignedSize($num as usize)), Type::SignedSize => Ok(Value::SignedSize($num as isize)),
+
+                    Type::Float32 => Ok(Value::Float32($num as f32)), Type::Float64 => Ok(Value::Float64($num as f64)),
+
+                    // Format onto a String
+                    Type::Str => Ok(Value::Str(format!("{}", $num))),
+
+                    // No other valid conversions
+                    _ => { Err(self) }
+                }
+            }
+        }
+
+        if let Type::Void = target {
+            Ok(Value::Void)
+        }else{
+            match self{
+                Value::Char(c) => if let Type::Str = target { Ok(Value::Str(c.escape_unicode().collect())) } else { Err(self) },
+
+                // Implement scalar casting for all number types
+                Value::Unsigned8(number)  => cast_number!(number), Value::Signed8(number) => cast_number!(number),
+                Value::Unsigned16(number) => cast_number!(number), Value::Signed16(number) => cast_number!(number),
+                Value::Unsigned32(number) => cast_number!(number), Value::Signed32(number) => cast_number!(number),
+                Value::Unsigned64(number) => cast_number!(number), Value::Signed64(number) => cast_number!(number),
+                Value::UnsignedSize(number) => cast_number!(number), Value::SignedSize(number) => cast_number!(number),
+                Value::Float32(number) => cast_number!(number), Value::Float64(number) => cast_number!(number),
+
+                _ => { Err(self) }
+            }
+        }
+    }
 }
 
-pub struct Function {
-    name: String,
+#[test]
+fn value(){
+    // Casting
+    let value = Value::Signed64(-159357789456123);
+
+    // Stingifying
+    assert_eq!(value.clone().try_cast(Type::Str).unwrap(), Value::Str("-159357789456123".to_owned()));
+
+    // Casting between signed and unsigned
+    assert_eq!(value.clone().try_cast(Type::Unsigned64).unwrap(), Value::Unsigned64(18446584715920095493));
 }
-impl Function {
-    pub fn call() -> Type {
-        Type::Void
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct Context{
+    variables: HashMap<Identifier, Value>,
+    functions: HashMap<Identifier, Function>,
+
+    stack: Vec<Context>
+}
+impl Context{
+    /// Saves the current state of the context
+    pub fn push(&mut self){
+        let copy = self.clone();
+        self.stack.push(copy);
+    }
+
+    /// Revers to a previously saved context discarding all changes
+    pub fn pop(&mut self){
+        let previous = self.stack.pop().expect("Tried to pop Context with empty stack");
+        *self = previous
+    }
+
+    /// Reverts to a previously saved context retaining the changes made to values present in it
+    pub fn merge_pop(&mut self){
+        let mut previous = self.stack.pop().expect("Tried to pop Context with empty stack");
+
+        for (key, value) in previous.variables.iter_mut(){
+            // Apply changes made to variables present on the previous stack
+            if let Some(new) = self.variables.remove(key){ *value = new }
+        }
+        for (key, value) in previous.functions.iter_mut(){
+            // Apply changes made to functions present on the previous stack
+            if let Some(new) = self.functions.remove(key){ *value = new }
+        }
+
+        *self = previous
+    }
+}
+
+use std::rc::Rc;
+use std::cell::RefCell;
+pub struct VirtualMachine{
+    memory: Vec<Rc<RefCell<Value>>>
+}
+impl VirtualMachine{
+    pub fn new() -> VirtualMachine{
+        VirtualMachine{ memory: Vec::new() }
     }
 }
