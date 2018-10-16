331295f0ee18445d0d3865a737a660d7dcc7283c
Rafael Rodrigues Nakano <lazpeng@gmail.com>
2018-10-16 16:47:26 -0300

    Removido código antigo

--- src/old/mod.rs
+++
@@ -1,8 +0,0 @@
-//! This module contains the old Birlscript code (pre 2.0)
-//! Be warned that the code is very poorly written (but the 2.0 is too-)
-//!
-//! Not sure how long I'll keep this, but for now to measure performance improvements and regressions
-
-pub mod interpreter;
-pub mod eval;
-pub mod nparser;
